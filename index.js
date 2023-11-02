1e1f6b504be3475c73c664742065422c45274a59
d3us <dnarino@gmail.com>
2023-11-02 11:58:58 -0500

    create server express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('first')
+const express = require('express');
+const app = express();
+const PORT =3000;
+
+app.get('/',(req, res)=>{
+    res.send('Hello world');
+});
+
+app.listen(PORT,()=>{
+    console.log(`app listening on port ${PORT}`);
+})
